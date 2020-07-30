feccfc1c014e63cee3f9c6115f0e9e6068511c5d
Levi Ryffel <levi.ryffel@gmail.com>
2020-07-30 16:44:10 +0200

    added newline correctly

--- js/index.js
+++ js/index.js
@@ -15,7 +15,7 @@ import("../pkg/index.js").catch(console.error).then(module => {
 
         document.getElementById("text_box").focus();
         document.getElementById("form").reset();
-        document.body.innerHTML += "\nPress ENTER or click Check before submitting a new equation.";
+        document.body.innerHTML += "<br>Press ENTER or click Check before submitting a new equation.";
     }
 
 });
