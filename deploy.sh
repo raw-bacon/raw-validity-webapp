79fc5c81d13bf56b4dfade43cccd8775c1948ff5
Levi Ryffel <levi.ryffel@gmail.com>
2020-07-30 14:53:53 +0200

    modified shell script

--- deploy.sh
+++ deploy.sh
@@ -7,4 +7,7 @@ node_modules/.bin/webpack --release
 git switch gh-pages
 git rm -rf *
 mv dist/* .
+git add .
+git commit -m "deployed using deploy.sh script"
+git push origin gh-pages
 git switch master
